993ef056563872394d6de87a3afcb83ca8787e9f
kantapro2020 <kantapro2020@gmail.com>
2020-06-10 22:52:40 +0900

    fix

--- app/src/main/java/com/example/ieiruforandroid/MainActivity.kt
+++ app/src/main/java/com/example/ieiruforandroid/MainActivity.kt
@@ -3,13 +3,42 @@ package com.example.ieiruforandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.location.*
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // 位置情報を取得できるクラス
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = FusedLocationProviderClient(this)
+
+        // どのような取得方法を要求
+        val locationRequest = LocationRequest().apply {
+            // 精度重視(電力大)と省電力重視(精度低)を両立するため2種類の更新間隔を指定
+            // 今回は公式のサンプル通りにする。
+            interval = 10000                                   // 最遅の更新間隔(但し正確ではない。)
+            fastestInterval = 5000                             // 最短の更新間隔
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY  // 精度重視
+        }
+
+        // コールバック
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                // 更新直後の位置が格納されているはず
+                val location = locationResult?.lastLocation ?: return
+                Toast.makeText(this@MainActivity,
+                    "緯度:${location.latitude}, 経度:${location.longitude}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // 位置情報を更新
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+
         setContentView(R.layout.activity_main)
     }
     fun btnSendOnClick(view: View) {
